f6d11113571d80908595b239761f0d0d6da115fb
borissvoboda <svoboda.boris@gmail.com>
2024-08-30 13:46:29 +0200

    F2: create logic in Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,6 +1,8 @@
 import { PlusIcon } from "@radix-ui/react-icons";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Form } from "./form";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const StyledDiv = styled.header`
     display: flex;
@@ -27,13 +29,34 @@ type HeaderProps = {
 
 export const Header = (props: HeaderProps) => {
     const { children } = props;
+    const [isFormVisible, setIsFormVisible] = useState<boolean>(false);
+
+    const onItemAdd = (label: string) => {};
+
+    const toggleFormVisibility = () => {
+        setIsFormVisible((prev) => !prev);
+    };
+
+    const onCreateItem = (inputValue) => {
+        console.log("Submit");
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ label: inputValue, isDone: false }),
+        };
+        fetch(`${apiUrl}/items`, requestOptions).then((response) => response.json());
+        // .then(data => this.setState({ postId: data.id }));
+
+        // setInputValue("");
+    };
 
     return (
         <StyledDiv>
             <h1>{children}</h1>
-            <button>
+            <button onClick={toggleFormVisibility}>
                 <PlusIcon />
             </button>
+            {isFormVisible ? <Form onSubmit={onCreateItem} /> : ""}
         </StyledDiv>
     );
 };
